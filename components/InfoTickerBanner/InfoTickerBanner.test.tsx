e28acb0d22eecb2750789a70923f9d6cebca3799
Bilaal@Maci7 <bilaalnadeem95@gmail.com>
2025-04-05 00:45:18 +0100

    test_fix(info-ticker-banner): mock FontAwesome5 icon to fix Jest test crash

--- components/InfoTickerBanner/InfoTickerBanner.test.tsx
+++ components/InfoTickerBanner/InfoTickerBanner.test.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 import { render } from '@testing-library/react-native';
 import InfoTickerBanner from './InfoTickerBanner';
 
+jest.mock('@expo/vector-icons', () => ({
+    FontAwesome5: () => null,
+}));
+
 const mockItems = [
     { id: '1', icon: 'truck', text: 'Free UK delivery over £25' },
     { id: '2', icon: 'box', text: 'Click & Collect now available' },
 ];
+
+
 
 describe('InfoTickerBanner', () => {
     it('renders multiple promo items with text', () => {
